ea258206e277f695a3053265a6961871585b91f5
elsonjha <81917710+elsonjha@users.noreply.github.com>
2021-04-08 14:41:54 +0530

    Update insertemployee.php

--- insertemployee.php
+++ insertemployee.php
@@ -12,45 +12,60 @@
         echo "connected successfully";
     }
 
-  
-  
-   $name =  $_POST["name"];
-   $gender =  $_POST["gender"];
-   $email  = $_POST['email'];
-   $DOB  = $_POST["dateofbirth"];
-   $Contact_No  = $_POST["contact"];
-   $department  = $_POST["department"];
+    $profileImageName = $_FILES["profileImage"]["name"];
+    // For image upload
+    $target_dir = "images/";
+    $target_file = $target_dir . basename($profileImageName);
+    // VALIDATION
+    // validate image size. Size is calculated in Bytes
+    // if($_FILES['profileImage']['size'] > 200000) {
+    //   $msg = "Image size should not be greated than 200Kb";
+    //   $msg_class = "alert-danger";
+    // }
+    // check if file exists
+    if(file_exists($target_file)) {
+      $msg = "File already exists";
+      $msg_class = "alert-danger";
+    }
+    // Upload image only if no errors
+    if (empty($error)) {
+      if(move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file)) {
+        $name =  $_POST["name"];
+        $gender =  $_POST["gender"];
+        $email  = $_POST['email'];
+        $DOB  = $_POST["dateofbirth"];
+        $Contact_No  = $_POST["contact"];
+        $department  = $_POST["department"];
 
+       $sql = "INSERT INTO `employee_details`(`name`, `gender`, `email`, `DOB`, `contact_no`, `department`, `profile_image`) VALUES ('$name','$gender','$email','$DOB','$Contact_No','$department','$profileImageName')";
 
-
-  $sql = "INSERT INTO `employee_details`(`name`, `gender`, `email`, `DOB`, `contact_no`, `department`) VALUES ('$name','$gender','$email','$DOB','$Contact_No','$department')";
-
-  
-   
-
-  //$sql = "INSERT INTO `employee` (`name`, `email`, `DOB`, `contact_no`) VALUES ('$name','$email','$DOB','$Contact_No')";
-
-
-   if (mysqli_query($con,$sql))
-  {
-     ?>
-   <script>
-     
-	  if(!alert('Employee added successfully.')){window.location = "admin.php?view_employee=view_employee";}
-   </script>
-   <?php
-       } 
-   else {
-    ?>
-     <script>
-       // window.location = "admin.php?view_employee=view_employee";
-          if(!alert("Can not add employee. Some error occured")){window.location = "admin.php?view_employee=view_employee";}
-   
-     </script>
-   <?php
-   }	 
-
-  
+        if(mysqli_query($conn, $sql)){
+          ?>
+          <script>
+            
+           if(!alert('Employee added successfully.')){window.location = "admin.php?view_employee=view_employee";}
+          </script>
+          <?php
+        } else {
+          ?>
+          <script>
+            // window.location = "admin.php?view_employee=view_employee";
+               if(!alert("Can not add employee. Some error occured")){window.location = "admin.php?view_employee=view_employee";}
+        
+          </script>
+        <?php
+        }
+      } else {
+        ?>
+        <script>
+          // window.location = "admin.php?view_employee=view_employee";
+             if(!alert("Can not add employee. File Upload Error")){window.location = "admin.php?view_employee=view_employee";}
+      
+        </script>
+      <?php
+      }
+    }
+ 
   }
 
 ?>
