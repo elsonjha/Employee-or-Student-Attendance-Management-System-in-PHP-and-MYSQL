8b1f596d5e5f14ae02d89610a8159fbc6029682c
elsonjha <81917710+elsonjha@users.noreply.github.com>
2021-04-08 15:33:02 +0530

    Update insertemployee.php

--- insertemployee.php
+++ insertemployee.php
@@ -58,7 +58,7 @@
       } else {
         ?>
         <script>
-         console.log('<?php echo $error; ?>');
+         console.log('<?php echo move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file); ?>');
           // window.location = "admin.php?view_employee=view_employee";
              if(!alert("Can not add employee. File Upload Error")){window.location = "admin.php?view_employee=view_employee";}
       
